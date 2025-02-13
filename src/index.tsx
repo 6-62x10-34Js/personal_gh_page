18c53b5bc67d8c4f811fc9241bd01df9e17d61d5
6-62x10-34Js <georg_bluethner@outlook.com>
2025-02-13 17:47:19 +0100

    Fix Routing, try 2

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename="/personal_gh_page/Home">
       <App />
     </HashRouter>
   </React.StrictMode>
